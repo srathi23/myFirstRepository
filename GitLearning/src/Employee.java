20a17f710cce08804ecf9d681e394b078ae9b8f7
shubhamrathi <shubhamrathi@localhost>
2022-10-15 22:45:05 +0530

    pushed project

---
+++ GitLearning/src/Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+	
+	private Integer id;
+	private String name;
+	
+	public Employee(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+}
