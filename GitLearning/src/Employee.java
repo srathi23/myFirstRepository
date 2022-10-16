4ed0ba02dcef7e508d95650823b9dbb03721cc94
srathi23 <shubhamrathi@localhost>
2022-10-17 00:21:01 +0530

    Added emailId field in Employee class

--- GitLearning/src/Employee.java
+++ GitLearning/src/Employee.java
@@ -3,11 +3,18 @@ public class Employee {
 	
 	private Integer id;
 	private String name;
+	private String emailId;
 	
-	public Employee(Integer id, String name) {
+	public Employee(Integer id, String name, String emailId) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", emailId=" + emailId + "]";
 	}
 	
 }
--- GitLearning/src/Test.java
+++ GitLearning/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello!");
-		Employee employee = new Employee(1, "Ankit");
+		Employee employee = new Employee(1, "Ankit", "ankit@gmail.com");
 		System.out.println(employee);
 	}
 
