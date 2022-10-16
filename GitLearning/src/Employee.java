3902716305ce65c0dbb50e56b49ca64b53deedd4
srathi23 <shubhamrathi@localhost>
2022-10-17 00:29:27 +0530

    Added Address object in Employee class

--- GitLearning/src/Employee.java
+++ GitLearning/src/Employee.java
@@ -4,17 +4,19 @@ public class Employee {
 	private Integer id;
 	private String name;
 	private String emailId;
+	private Address address;
 	
-	public Employee(Integer id, String name, String emailId) {
+	public Employee(Integer id, String name, String emailId, Address address) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.emailId = emailId;
+		this.address = address;
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", emailId=" + emailId + "]";
+		return "Employee [id=" + id + ", name=" + name + ", emailId=" + emailId + ", address=" + address + "]";
 	}
 	
 }
--- GitLearning/src/Test.java
+++ GitLearning/src/Test.java
@@ -3,7 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello!");
-		Employee employee = new Employee(1, "Ankit", "ankit@gmail.com");
+		Address address = new Address("addressLine1", "addressLine2", "Pune", 123456);
+		Employee employee = new Employee(1, "Ankit", "ankit@gmail.com", address);
 		System.out.println(employee);
 	}
 
